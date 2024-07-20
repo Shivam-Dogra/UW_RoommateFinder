7ea64f8514f5e68fb41230e52a28198a18ecb72d
Shivam Dogra <shivamjdogra@gmail.com>
2024-07-20 16:20:38 -0400

    explore windsor feature added

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import RegisterUserPage from './components/RegisterUserPage';
+import HomePage from './components/HomePage';
+import NewUserPage from './components/NewUserPage';
+import ExploreWindsor from './components/ExploreWindsor';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterUserPage />} />
+        <Route path="/newuser" element={<NewUserPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/explorewindsor" element={<ExploreWindsor/>}/>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
