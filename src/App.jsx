019850376ac355f53afb28f87e110317b4d09025
Anisha Miyan <anishamiyan@Anishas-Laptop.local>
2024-07-22 18:20:55 -0400

    view group feature

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import RegisterUserPage from './components/RegisterUserPage';
 import HomePage from './components/HomePage';
 import NewUserPage from './components/NewUserPage';
 import ExploreWindsor from './components/ExploreWindsor';
+import ViewGroupsPage from './components/ViewGroupsPage';
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/newuser" element={<NewUserPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/explorewindsor" element={<ExploreWindsor/>}/>
+        <Route path="/viewgroups" element ={<ViewGroupsPage/>}/>
       </Routes>
     </Router>
   );
--- src/components/ViewGroupsPage.jsx
+++ src/components/ViewGroupsPage.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import ProfileCard from './ProfileCard';
-import { users } from '../assets/profile';  
-import LandingSection from './LandingSection';
 import { useNavigate } from 'react-router-dom';
+import { groups } from '../assets/group.js';
+import GroupCard from './GroupCard';
 
-const HomePage = () => {
+const ViewGroupsPage = () => {
   const navigate = useNavigate();
+
+  const handleJoinGroup = (groupId) => {
+    console.log('Joining group with ID:', groupId);
+  };
 
   return (
     <div className="bg-gradient-to-br from-red-500 to-red-700 min-h-screen">
       <nav className="bg-red-800 text-white p-4 shadow-md">
         <div className="container mx-auto flex justify-between items-center">
           <h1 className="font-bold text-xl">Roommate Finder 🔎</h1>
-          <input type="text" placeholder="Search Group..." className="p-2 rounded bg-white border-transparent focus:border-gray-500 focus:bg-white focus:ring-0" />
           <div>
             <button 
               className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 mx-5 rounded transition duration-300 ease-in-out"  
@@ -36,19 +38,14 @@ const HomePage = () => {
       </nav>
 
       <main className="container mx-auto p-4 text-black">
-        <LandingSection />
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-8">
-          {users.map(user => <ProfileCard key={user.name} user={user} />)}
+          {groups.map(group => (
+            <GroupCard key={group.id} group={group} onJoin={handleJoinGroup} />
+          ))}
         </div>
       </main>
-       <div className="bg-red-500 text-center py-10 mt-12">
-          <h2 className="text-white text-xl font-semibold mb-4">Don't do college alone.</h2>
-          <button className="text-red-700 bg-white font-bold py-3 px-6 rounded hover:bg-gray-200">
-            Share with friends →
-          </button>
-        </div>
     </div>
   );
 };
 
-export default HomePage;
+export default ViewGroupsPage;
